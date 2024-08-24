672eeead595a29cdfa2b9535ab835e550de793f0
reuel.bandeira <reuel.bandeira@grupoicts.com.br>
2024-08-24 14:23:05 -0400

    feat: add orm config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,20 +1,21 @@
 import * as dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 export default {
   type: 'mysql',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   synchronize: false,
-  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-  migrations: ['./src/shared/infra/typeorm/database/migrations/*.ts'],
+  entities: [path.join(__dirname, '..', '..', 'modules', '**', 'infra', 'typeorm', 'entities', '*.ts')],
+  migrations: [path.join(__dirname, '..', 'shared', 'infra', 'typeorm', 'database', 'migrations', '*.ts')],
   cli: {
-    entitiesDir: './src/modules/**/infra/typeorm/entities',
-    migrationsDir: './src/shared/infra/typeorm/database/migrations',
+    entitiesDir: path.join('src', 'modules', '**', 'infra', 'typeorm', 'entities'),
+    migrationsDir: path.join('src', 'shared', 'infra', 'typeorm', 'database', 'migrations'),
   },
   timezone: 'Z',
 };
